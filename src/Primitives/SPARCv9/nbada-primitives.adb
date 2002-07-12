cfc3c93e72715987afb268049862a76061347ce0
andersg <andersg>
2002-07-12 15:02:45 +0000

    Initial version.

---
+++ src/Primitives/SPARCv9/nbada-primitives.adb
@@ -0,0 +1,191 @@
+-------------------------------------------------------------------------------
+--                              -*- Mode: Ada -*-
+-- Filename        : primitives.adb
+-- Description     : Synchronization primitives.
+-- Author          : Anders Gidenstam
+-- Created On      : Fri Jul  5 14:53:50 2002
+-- $Id: nbada-primitives.adb,v 1.1 2002/07/12 15:01:57 andersg Exp $
+-------------------------------------------------------------------------------
+
+with System.Machine_Code;
+with Ada.Characters.Latin_1;
+
+package body Primitives is
+
+   ----------------------------------------------------------------------------
+   -- Synchronization primitives for SPARC v9.
+   --
+   -- Requires the flags '-cargs -Wa,-xarch=v8plus' to gnatmake.
+   ----------------------------------------------------------------------------
+
+   ----------------------------------------------------------------------------
+   procedure Compare_And_Swap_32 (Target    : access Element;
+                                  Old_Value : in     Element;
+                                  New_Value : in out Element) is
+      use Ada.Characters.Latin_1;
+      pragma Assert (Element'Object_Size = 32);
+      type Element_Access is access all Element;
+   begin
+      System.Machine_Code.Asm
+        (Template =>
+           "!#BEGIN Compare_And_Swap_32" & LF & HT &
+           "mov %3, %%l0"                & LF & HT &   -- l0 <- %3
+           "cas [%1], %2, %%l0"          & LF & HT &   -- Compare & swap
+           "mov %%l0, %0"                & LF & HT &   -- %0 <- l0
+           "!#END Compare_And_Swap_32",
+         Outputs  => Element'Asm_Output ("=r", New_Value), -- %0 = New_Value
+         Inputs   => (Element_Access'Asm_Input ("r",       -- %1 = Target
+                                                Element_Access (Target)),
+                      Element'Asm_Input ("r", Old_Value),  -- %2 = Old_Value
+                      Element'Asm_Input ("r", New_Value)), -- %3 = New_Value
+         Clobber  => "l0",
+         Volatile => True);
+   end Compare_And_Swap_32;
+
+   ----------------------------------------------------------------------------
+   function Boolean_Compare_And_Swap_32 (Target    : access Element;
+                                         Old_Value : in     Element;
+                                         New_Value : in     Element)
+                                        return Boolean is
+      use Ada.Characters.Latin_1;
+      pragma Assert (Element'Object_Size = 32);
+      type Element_Access is access all Element;
+      Tmp : Element;
+   begin
+      System.Machine_Code.Asm
+        (Template =>
+           "!#BEGIN Boolean_Compare_And_Swap_32" & LF & HT &
+           "mov %3, %%l0"                & LF & HT &   -- l0 <- %3
+           "cas [%1], %2, %%l0"          & LF & HT &   -- Compare & swap
+           "mov %%l0, %0"                & LF & HT &   -- %0 <- l0
+           "!#END Boolean_Compare_And_Swap_32",
+         Outputs  => Element'Asm_Output ("=r", Tmp),       -- %0 = Tmp
+         Inputs   => (Element_Access'Asm_Input ("r",       -- %1 = Target
+                                                Element_Access (Target)),
+                      Element'Asm_Input ("r", Old_Value),  -- %2 = Old_Value
+                      Element'Asm_Input ("r", New_Value)), -- %3 = New_Value
+         Clobber  => "l0",
+         Volatile => True);
+      return Tmp = Old_Value;
+   end Boolean_Compare_And_Swap_32;
+
+   ----------------------------------------------------------------------------
+   -- My GCC does not generate 64-bit aware code, so 64-bit objects cannot be
+   -- handled atomically.
+   ----------------------------------------------------------------------------
+
+--    ----------------------------------------------------------------------------
+--    procedure Compare_And_Swap_64 (Target    : access Element;
+--                                   Old_Value : in     Element;
+--                                   New_Value : in out Element) is
+--       use Ada.Characters.Latin_1;
+--       pragma Assert (Element'Object_Size = 64);
+--       type Element_Access is access all Element;
+--    begin
+--       System.Machine_Code.Asm
+--         (Template =>
+--            "!#BEGIN Compare_And_Swap_64" & LF & HT &
+--            "mov %3, %%l0"                & LF & HT &   -- l0 <- %3
+--            "casx [%1], %2, %%l0"         & LF & HT &   -- Compare & swap
+--            "mov %%l0, %0"                & LF & HT &   -- %0 <- l0
+--            "!#END Compare_And_Swap_64",
+--          Outputs  => Element'Asm_Output ("=r", New_Value), -- %0 = New_Value
+--          Inputs   => (Element_Access'Asm_Input ("r",       -- %1 = Target
+--                                                 Element_Access (Target)),
+--                       Element'Asm_Input ("r", Old_Value),  -- %2 = Old_Value
+--                       Element'Asm_Input ("r", New_Value)), -- %3 = New_Value
+--          Clobber  => "l0",
+--          Volatile => True);
+--    end Compare_And_Swap_64;
+
+--    ----------------------------------------------------------------------------
+--    function Boolean_Compare_And_Swap_64 (Target    : access Element;
+--                                          Old_Value : in     Element;
+--                                          New_Value : in     Element)
+--                                         return Boolean is
+--       use Ada.Characters.Latin_1;
+--       pragma Assert (Element'Object_Size = 64);
+--       type Element_Access is access all Element;
+--       Tmp : Element;
+--    begin
+--       System.Machine_Code.Asm
+--         (Template =>
+--            "!#BEGIN Boolean_Compare_And_Swap_64" & LF & HT &
+--            "mov %3, %%l0"                & LF & HT &   -- l0 <- %3
+--            "cas [%1], %2, %%l0"          & LF & HT &   -- Compare & swap
+--            "mov %%l0, %0"                & LF & HT &   -- %0 <- l0
+--            "!#END Boolean_Compare_And_Swap_64",
+--          Outputs  => Element'Asm_Output ("=r", Tmp),       -- %0 = Tmp
+--          Inputs   => (Element_Access'Asm_Input ("r",       -- %1 = Target
+--                                                 Element_Access (Target)),
+--                       Element'Asm_Input ("r", Old_Value),  -- %2 = Old_Value
+--                       Element'Asm_Input ("r", New_Value)), -- %3 = New_Value
+--          Clobber  => "l0",
+--          Volatile => True);
+--       return Tmp = Old_Value;
+--    end Boolean_Compare_And_Swap_64;
+
+   ----------------------------------------------------------------------------
+   procedure Fetch_And_Add (Target    : access Unsigned_32;
+                            Increment : in     Unsigned_32) is
+      use Ada.Characters.Latin_1;
+      type Unsigned_32_Access is access all Unsigned_32;
+   begin
+      System.Machine_Code.Asm
+        (Template =>
+           "!#BEGIN Fetch_And_Add_32"      & LF &
+           "retry1:"                       & LF & HT &   -- retry:
+           "ld [%0], %%l0"                 & LF & HT &   --  l0 <- [%0]
+           "add %%l0, %1, %%l1"            & LF & HT &   --  l1 <- l0 + %1
+           "cas [%0], %%l0, %%l1"          & LF & HT &   --  cas [%0] l0 l1
+           "cmp %%l0, %%l1"                & LF & HT &   --  if l0 /= l1
+           "bne retry1"                    & LF & HT &   --   goto retry
+           "nop"                           & LF & HT &
+           "!#END Fetch_And_Add_32",
+         Inputs   => (Unsigned_32_Access'Asm_Input         -- %0 = Target
+                      ("r", Unsigned_32_Access (Target)),
+                      Unsigned_32'Asm_Input ("r",          -- %1 = Increment
+                                             Increment)),
+         Clobber  => "l0,l1",
+         Volatile => True);
+   end Fetch_And_Add;
+
+   ----------------------------------------------------------------------------
+   function Fetch_And_Add (Target    : access Unsigned_32;
+                           Increment : in     Unsigned_32)
+                          return Unsigned_32 is
+      use Ada.Characters.Latin_1;
+      type Unsigned_32_Access is access all Unsigned_32;
+
+      Tmp : Unsigned_32;
+   begin
+      System.Machine_Code.Asm
+        (Template =>
+           "!#BEGIN Fetch_And_Add_32"      & LF &
+           "retry2:"                       & LF & HT &   -- retry:
+           "ld [%1], %%l0"                 & LF & HT &   --  l0 <- [%1]
+           "add %%l0, %2, %%l1"            & LF & HT &   --  l1 <- l0 + %2
+           "cas [%1], %%l0, %%l1"          & LF & HT &   --  cas [%1] l0 l1
+           "cmp %%l0, %%l1"                & LF & HT &   --  if l0 /= l1
+           "bne retry2"                    & LF & HT &   --   goto retry
+           "mov %%l1, %0"                  & LF & HT &   --  %0 <- l1
+           "!#END Fetch_And_Add_32",
+         Outputs  => Unsigned_32'Asm_Output ("=r", Tmp),   -- %0 = Tmp
+         Inputs   => (Unsigned_32_Access'Asm_Input         -- %1 = Target
+                      ("r", Unsigned_32_Access (Target)),
+                      Unsigned_32'Asm_Input ("r",          -- %2 = Increment
+                                             Increment)),
+         Clobber  => "l0,l1",
+         Volatile => True);
+      return Tmp;
+   end Fetch_And_Add;
+
+   ----------------------------------------------------------------------------
+   procedure Membar_StoreLoad_StoreStore is
+   begin
+      System.Machine_Code.Asm
+        (Template =>
+           "membar #StoreLoad | #StoreStore");
+   end Membar_StoreLoad_StoreStore;
+
+end Primitives;
---
+++ src/Primitives/nbada-primitives.ads
@@ -0,0 +1,97 @@
+-------------------------------------------------------------------------------
+--                              -*- Mode: Ada -*-
+-- Filename        : primitives.ads
+-- Description     : Synchronization primitives.
+-- Author          : Anders Gidenstam
+-- Created On      : Fri Jul  5 12:27:13 2002
+-- $Id: nbada-primitives.ads,v 1.1 2002/07/12 15:01:53 andersg Exp $
+-------------------------------------------------------------------------------
+
+with Interfaces;
+
+package Primitives is
+
+   ----------------------------------------------------------------------------
+   -- Synchronization primitives.
+   --
+   --
+   ----------------------------------------------------------------------------
+
+   ----------------------------------------------------------------------------
+   -- Not_Implemented is raised if a particular primitive is not implemented
+   -- on the platform in use.
+   Not_Implemented : exception;
+
+   ----------------------------------------------------------------------------
+   -- 32 bit unsigned word.
+   type Unsigned_32 is mod 2**32;
+   pragma Atomic (Unsigned_32);
+
+   ----------------------------------------------------------------------------
+   -- Compare and Swap 32
+   generic
+      -- Use pragma Atomic and pragma Volatile for Target.
+      -- Element'Object_Size MUST be 32.
+      type Element is private;
+   procedure Compare_And_Swap_32 (Target    : access Element;
+                                  Old_Value : in     Element;
+                                  New_Value : in out Element);
+   pragma Inline (Compare_And_Swap_32);
+   pragma Inline_Always (Compare_And_Swap_32);
+
+   ----------------------------------------------------------------------------
+   -- Boolean Compare and Swap 32
+   generic
+      -- Use pragma Atomic and pragma Volatile for Target.
+      -- Element'Object_Size MUST be 32.
+      type Element is private;
+   function Boolean_Compare_And_Swap_32 (Target    : access Element;
+                                         Old_Value : in     Element;
+                                         New_Value : in     Element)
+                                        return Boolean;
+   pragma Inline (Boolean_Compare_And_Swap_32);
+   pragma Inline_Always (Boolean_Compare_And_Swap_32);
+
+--    ----------------------------------------------------------------------------
+--    -- Compare and Swap 64
+--    generic
+--       -- Use pragma Atomic and pragma Volatile for Target.
+--       -- Element'Object_Size MUST be 64.
+--       type Element is private;
+--    procedure Compare_And_Swap_64 (Target    : access Element;
+--                                   Old_Value : in     Element;
+--                                   New_Value : in out Element);
+--    pragma Inline (Compare_And_Swap_64);
+--    pragma Inline_Always (Boolean_Compare_And_Swap_32);
+
+--    ----------------------------------------------------------------------------
+--    -- Boolean Compare and Swap 64
+--    generic
+--       -- Use pragma Atomic and pragma Volatile for Target.
+--       -- Element'Object_Size MUST be 64.
+--       type Element is private;
+--    function Boolean_Compare_And_Swap_64 (Target    : access Element;
+--                                          Old_Value : in     Element;
+--                                          New_Value : in     Element)
+--                                         return Boolean;
+--    pragma Inline (Boolean_Compare_And_Swap_64);
+--    pragma Inline_Always (Boolean_Compare_And_Swap_32);
+
+   ----------------------------------------------------------------------------
+   -- Fetch and Add
+   -- Use pragma Volatile for Target.all.
+   procedure Fetch_And_Add (Target    : access Unsigned_32;
+                            Increment : in     Unsigned_32);
+   pragma Inline (Fetch_And_Add);
+   pragma Inline_Always (Fetch_And_Add);
+   function  Fetch_And_Add (Target    : access Unsigned_32;
+                            Increment : in     Unsigned_32)
+                           return Unsigned_32;
+   pragma Inline (Fetch_And_Add);
+   pragma Inline_Always (Fetch_And_Add);
+
+   ----------------------------------------------------------------------------
+   -- Memory barriers
+   procedure Membar_StoreLoad_StoreStore;
+
+end Primitives;
