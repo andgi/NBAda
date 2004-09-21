99c3668aac93fddd236a05226267ed0f6ff09984
anders <anders>
2004-09-21 20:13:50 +0000

    Added GPL license.

--- src/Primitives/IA32/nbada-primitives.adb
+++ src/Primitives/IA32/nbada-primitives.adb
@@ -1,10 +1,34 @@
 -------------------------------------------------------------------------------
+-- Primitives - A binding to the synchronization primitives of the hardware.
+-- Copyright (C) 2004  Anders Gidenstam
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+-------------------------------------------------------------------------------
 --                              -*- Mode: Ada -*-
 -- Filename        : primitives.adb
 -- Description     : Synchronization primitives.
 -- Author          : Anders Gidenstam
 -- Created On      : Fri Jul  5 14:53:50 2002
--- $Id: nbada-primitives.adb,v 1.3 2004/09/20 23:18:13 anders Exp $
+-- $Id: nbada-primitives.adb,v 1.4 2004/09/21 20:13:50 anders Exp $
 -------------------------------------------------------------------------------
 
 with System.Machine_Code;
@@ -13,13 +37,13 @@ with Ada.Characters.Latin_1;
 package body Primitives is
 
    ----------------------------------------------------------------------------
-   -- Synchronization primitives for IA32.
+   --  Synchronization primitives for IA32.
    --
    ----------------------------------------------------------------------------
 
    ----------------------------------------------------------------------------
-   -- Home made assert construction. Provides some degree of compile time
-   -- checking.
+   --  Home made assert construction. Provides some degree of compile time
+   --  checking.
    subtype Always_True is Boolean range True .. True;
    type Assertion (Assert : Always_True) is
      null record;
@@ -80,8 +104,8 @@ package body Primitives is
    end Boolean_Compare_And_Swap_32;
 
    ----------------------------------------------------------------------------
-   -- My GCC does not generate 64-bit aware code, so 64-bit objects cannot be
-   -- handled atomically. Not tested on IA32 yet.
+   --  The newer IA32 CPUs supports atomic operations on 64 bit objects.
+   --  However, I have not implememented or tested this yet.
    ----------------------------------------------------------------------------
 
    ----------------------------------------------------------------------------
@@ -93,21 +117,7 @@ package body Primitives is
 
       A1 : Assertion (Assert => Element'Object_Size = 64);
    begin
-      System.Machine_Code.Asm
-        (Template =>
-           "#BEGIN Compare_And_Swap_32" & LF & HT &
---         "movl %2, %%eax"              & LF & HT &
---            "lock"                        & LF & HT &
---         "cmpxchg8b %3, (%1)"          & LF & HT &   -- Compare & swap
---         "movl %%eax, %0"              & LF & HT &
-           "#END Compare_And_Swap_32",
-         Outputs  => Element'Asm_Output ("=r", New_Value), -- %0 = New_Value
-         Inputs   => (Element_Access'Asm_Input ("r",       -- %1 = Target
-                                                Element_Access (Target)),
-                      Element'Asm_Input ("r", Old_Value),  -- %2 = Old_Value
-                      Element'Asm_Input ("r", New_Value)), -- %3 = New_Value
-         Clobber  => "eax",
-         Volatile => True);
+      raise Not_Implemented;
    end Compare_And_Swap_64;
 
    ----------------------------------------------------------------------------
@@ -121,22 +131,7 @@ package body Primitives is
       A1 : Assertion (Assert => Element'Object_Size = 64);
       Tmp : Element;
    begin
-      System.Machine_Code.Asm
-        (Template =>
-           "#BEGIN Compare_And_Swap_32" & LF & HT &
---         "movl %2, %%eax"              & LF & HT &
---            "lock"                        & LF & HT &
---         "cmpxchg8b %3, (%1)"          & LF & HT &   -- Compare & swap
---         "movl %%eax, %0"              & LF & HT &
-           "#END Compare_And_Swap_32",
-         Outputs  => Element'Asm_Output ("=r", Tmp),       -- %0 = Tmp
-         Inputs   => (Element_Access'Asm_Input ("r",       -- %1 = Target
-                                                Element_Access (Target)),
-                      Element'Asm_Input ("r", Old_Value),  -- %2 = Old_Value
-                      Element'Asm_Input ("r", New_Value)), -- %3 = New_Value
-         Clobber  => "eax",
-         Volatile => True);
-      return Tmp = Old_Value;
+      raise Not_Implemented;
    end Boolean_Compare_And_Swap_64;
 
    ----------------------------------------------------------------------------
@@ -186,20 +181,10 @@ package body Primitives is
    ----------------------------------------------------------------------------
    procedure Membar is
    begin
+      --  Currently I do not know if there are any IA32
+      --  multiprocessors that have weak(er) memory consistency and
+      --  need memory barriers.
       null;
    end Membar;
-
-   ----------------------------------------------------------------------------
-   procedure Membar_StoreLoad is
-   begin
-      null;
-   end Membar_StoreLoad;
-
-   ----------------------------------------------------------------------------
-   procedure Membar_StoreStore_LoadStore is
-   begin
-      null;
-   end Membar_StoreStore_LoadStore;
-
 
 end Primitives;
