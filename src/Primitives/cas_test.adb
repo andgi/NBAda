57096bc8b9a35b9c092f4e0c9f95c8665850771d
anders <anders>
2004-09-21 11:56:56 +0000

    *** empty log message ***

--- src/Primitives/cas_test.adb
+++ src/Primitives/cas_test.adb
@@ -4,7 +4,7 @@
 -- Description     : Test of synchronization primitives package.
 -- Author          : Anders Gidenstam
 -- Created On      : Fri Jul  5 16:09:25 2002
--- $Id: cas_test.adb,v 1.2 2003/12/13 17:25:48 andersg Exp $
+-- $Id: cas_test.adb,v 1.3 2004/09/21 11:56:56 anders Exp $
 -------------------------------------------------------------------------------
 
 with Ada.Text_IO;
@@ -41,14 +41,39 @@ begin
    Ada.Text_IO.Put_Line ("A: " & Integer'Image (A));
    Ada.Text_IO.Put_Line ("B: " & Integer'Image (B));
    Ada.Text_IO.Put_Line ("C: " & Integer'Image (C));
+
+   --  Test 1.
    Ada.Text_IO.Put_Line ("CAS (A, B, C)");
    CAS (A'Access,
         Old_Value => B,
         New_Value => C);
-   if A /= 2 then
+   if A = 2 and C = 77 then
+      Ada.Text_IO.Put_Line ("Compare_And_Swap_32: Passed test 1.");
+   else
+      Ada.Text_IO.Put_Line ("Compare_And_Swap_32 is faulty!");
+      Ada.Text_IO.Put_Line ("A: " & Integer'Image (A));
+      Ada.Text_IO.Put_Line ("B: " & Integer'Image (B));
+      Ada.Text_IO.Put_Line ("C: " & Integer'Image (C));
+   end if;
+
+   Ada.Text_IO.Put_Line ("----------------------");
+   -- Test 2.
+   C := 8;
+   Ada.Text_IO.Put_Line ("A: " & Integer'Image (A));
+   Ada.Text_IO.Put_Line ("C: " & Integer'Image (C));
+   Ada.Text_IO.Put_Line ("CAS (A, 3, C)");
+   CAS (A'Access,
+        Old_Value => 3,
+        New_Value => C);
+   if A = 2 and C = 2 then
+      Ada.Text_IO.Put_Line ("Compare_And_Swap_32: Passed test 2.");
+   else
       Ada.Text_IO.Put_Line ("Compare_And_Swap_32 is faulty!");
    end if;
 
+   Ada.Text_IO.Put_Line ("----------------------");
+   -- Test 3.
+   C := 5;
    Ada.Text_IO.Put_Line ("A: " & Integer'Image (A));
    Ada.Text_IO.Put_Line ("B: " & Integer'Image (B));
    Ada.Text_IO.Put_Line ("C: " & Integer'Image (C));
@@ -56,12 +81,29 @@ begin
    Ada.Text_IO.Put_Line ("CAS (A, 2, B)");
    if (CAS (A'Access,
             Old_Value => 2,
-            New_Value => C)) then
-      Ada.Text_IO.Put_Line ("A: " & Integer'Image (A));
+            New_Value => C)) and then A = 5 then
+      Ada.Text_IO.Put_Line ("Boolean_Compare_And_Swap_32: Passed test 3.");
    else
       Ada.Text_IO.Put_Line ("Boolean_Compare_And_Swap_32 is faulty!");
    end if;
 
+   Ada.Text_IO.Put_Line ("----------------------");
+   -- Test 4.
+   Ada.Text_IO.Put_Line ("A: " & Integer'Image (A));
+   Ada.Text_IO.Put_Line ("B: " & Integer'Image (B));
+   Ada.Text_IO.Put_Line ("C: " & Integer'Image (C));
+
+   Ada.Text_IO.Put_Line ("CAS (A, 2, B)");
+   if (CAS (A'Access,
+            Old_Value => 2,
+            New_Value => C)) or else A /= 5 then
+      Ada.Text_IO.Put_Line ("Boolean_Compare_And_Swap_32 is faulty!");
+   else
+      Ada.Text_IO.Put_Line ("Boolean_Compare_And_Swap_32: Passed test 4.");
+   end if;
+
+   Ada.Text_IO.Put_Line ("----------------------");
+   -- Test 5.
    declare
       LA : aliased My_Float := -2.0;
       LB : My_Float := 2.0;
