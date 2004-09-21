127907319dd09c2e9f8353380e0e1515f38bd126
anders <anders>
2004-09-21 20:24:54 +0000

    GPL license.

--- src/Primitives/cas_test.adb
+++ src/Primitives/cas_test.adb
@@ -1,10 +1,34 @@
 -------------------------------------------------------------------------------
+-- Compare and Swap test.
+-- Copyright (C) 2004  Anders Gidenstam
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+-- As a special exception, if other files instantiate generics from this
+-- unit, or you link this unit with other files to produce an executable,
+-- this unit does not by itself cause the resulting executable to be
+-- covered by the GNU General Public License. This exception does not
+-- however invalidate any other reasons why the executable file might be
+-- covered by the GNU Public License.
+-------------------------------------------------------------------------------
 --                              -*- Mode: Ada -*-
 -- Filename        : cas_test.adb
 -- Description     : Test of synchronization primitives package.
 -- Author          : Anders Gidenstam
 -- Created On      : Fri Jul  5 16:09:25 2002
--- $Id: cas_test.adb,v 1.3 2004/09/21 11:56:56 anders Exp $
+-- $Id: cas_test.adb,v 1.4 2004/09/21 20:24:54 anders Exp $
 -------------------------------------------------------------------------------
 
 with Ada.Text_IO;
