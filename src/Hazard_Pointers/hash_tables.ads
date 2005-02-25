4b0f61e7eebeebfd51a2afc35ce615c677dfbf52
anders <anders>
2005-02-25 16:26:04 +0000

    Added the GPL.

--- src/Hazard_Pointers/hash_tables.ads
+++ src/Hazard_Pointers/hash_tables.ads
@@ -1,10 +1,35 @@
 -------------------------------------------------------------------------------
+--  Hazard Pointers - An implementation of Maged Michael's hazard pointers.
+--  Copyright (C) 2004, 2005  Anders Gidenstam
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+--  As a special exception, if other files instantiate generics from this
+--  unit, or you link this unit with other files to produce an executable,
+--  this unit does not by itself cause the resulting executable to be
+--  covered by the GNU General Public License. This exception does not
+--  however invalidate any other reasons why the executable file might be
+--  covered by the GNU Public License.
+--
+-------------------------------------------------------------------------------
 --                              -*- Mode: Ada -*-
--- Filename        : hash_tables.ads
--- Description     : A simple closed hash table.
--- Author          : Anders Gidenstam
--- Created On      : Thu Nov 25 21:51:42 2004
--- $Id: hash_tables.ads,v 1.1 2004/11/25 22:56:53 anders Exp $
+--  Filename        : hash_tables.ads
+--  Description     : A simple closed hash table.
+--  Author          : Anders Gidenstam
+--  Created On      : Thu Nov 25 21:51:42 2004
+--  $Id: hash_tables.ads,v 1.2 2005/02/25 16:26:04 anders Exp $
 -------------------------------------------------------------------------------
 
 generic
