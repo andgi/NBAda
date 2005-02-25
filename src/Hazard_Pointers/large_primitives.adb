1856b4eab5651dba47e0c5fdc33486f34969432f
anders <anders>
2005-02-25 16:30:27 +0000

    *** empty log message ***

--- src/Hazard_Pointers/large_primitives.adb
+++ src/Hazard_Pointers/large_primitives.adb
@@ -33,11 +33,11 @@
 --                     Implementations Using 64-Bit CAS".
 --  Author          : Anders Gidenstam
 --  Created On      : Thu Feb 24 10:25:44 2005
---  $Id: large_primitives.adb,v 1.3 2005/02/25 16:16:12 anders Exp $
+--  $Id: large_primitives.adb,v 1.4 2005/02/25 16:30:27 anders Exp $
 -------------------------------------------------------------------------------
 
 with Ada.Unchecked_Conversion;
-with Lock_Free_Fixed_Size_Storage_Pools;
+--with Lock_Free_Fixed_Size_Storage_Pools;
 
 with Primitives;
 with Ada.Text_IO;
