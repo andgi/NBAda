c3eb1738ac6c547abccdf988cde30b2b05257bf7
anders <anders>
2005-06-10 14:31:09 +0000

    Updated to use version tags to prevent ABA problems.

--- src/Lock-Free_Storage_Pools/nbada-lock_free_fixed_size_storage_pools.adb
+++ src/Lock-Free_Storage_Pools/nbada-lock_free_fixed_size_storage_pools.adb
@@ -1,10 +1,34 @@
 -------------------------------------------------------------------------------
+--  Lock-free fixed size storage pool.
+--  Copyright (C) 2003 - 2005  Anders Gidenstam
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+--  As a special exception, if other files instantiate generics from this
+--  unit, or you link this unit with other files to produce an executable,
+--  this unit does not by itself cause the resulting executable to be
+--  covered by the GNU General Public License. This exception does not
+--  however invalidate any other reasons why the executable file might be
+--  covered by the GNU Public License.
+-------------------------------------------------------------------------------
 --                              -*- Mode: Ada -*-
--- Filename        : lock_free_fixed_size_storage_pools.adb
--- Description     : A lock-free fixed size storage pool implementation.
--- Author          : Anders Gidenstam
--- Created On      : Thu Apr  3 17:50:52 2003
--- $Id: nbada-lock_free_fixed_size_storage_pools.adb,v 1.2 2005/06/10 08:58:21 andersg Exp $
+--  Filename        : lock_free_fixed_size_storage_pools.adb
+--  Description     : A lock-free fixed size storage pool implementation.
+--  Author          : Anders Gidenstam
+--  Created On      : Thu Apr  3 17:50:52 2003
+--  $Id: nbada-lock_free_fixed_size_storage_pools.adb,v 1.3 2005/06/10 14:31:09 anders Exp $
 -------------------------------------------------------------------------------
 
 with Ada.Unchecked_Deallocation;
@@ -14,21 +38,31 @@ with Primitives;
 package body Lock_Free_Fixed_Size_Storage_Pools is
 
    ----------------------------------------------------------------------------
+   type Pool_Block_Access is access all Pool_Block;
+
+   function To_Block_Access (X    : Pool_Block_Ref;
+                             Pool : Lock_Free_Storage_Pool)
+                            return Pool_Block_Access;
+   function To_Block_Ref    (X    : Pool_Block_Access;
+                             Ver  : Version_Number;
+                             Pool : Lock_Free_Storage_Pool)
+                            return Pool_Block_Ref;
+   function Is_Null (X : Pool_Block_Ref) return Boolean;
+
    procedure Free is
       new Ada.Unchecked_Deallocation (System.Storage_Elements.Storage_Array,
                                       Storage_Array_Access);
+   function CAS is new
+     Primitives.Boolean_Compare_And_Swap_32 (Pool_Block_Ref);
 
    ----------------------------------------------------------------------------
    package Pool_Blocks is
       new System.Address_To_Access_Conversions (Pool_Block);
 
-   ----------------------------------------------------------------------------
-   function CAS is new
-     Primitives.Boolean_Compare_And_Swap_32 (Pool_Block_Access);
 
    ----------------------------------------------------------------------------
-   -- Home made assert construction. Provides some degree of compile time
-   -- checking.
+   --  Home made assert construction. Provides some degree of compile time
+   --  checking.
    subtype Always_True is Boolean range True .. True;
    type Assertion (Assert : Always_True) is
      null record;
@@ -49,47 +83,34 @@ package body Lock_Free_Fixed_Size_Storage_Pools is
       end if;
 
       loop
-         Block := Pool.Free_List;
-
-         if Block = null then
-            raise Storage_Exhausted;
-         end if;
          declare
-            use type System.Address;
+            Block_Ref : constant Pool_Block_Ref := Pool.Free_List;
          begin
-            if Block.all'Address < Pool.Storage (Pool.Storage'First)'Address or
-              Block.all'Address > Pool.Storage (Pool.Storage'Last)'Address
-            then
-               raise Implementation_Error;
+            if Is_Null (Block_Ref) then
+               raise Storage_Exhausted;
             end if;
-         end;
-         declare
-            use type System.Address;
-         begin
-            if Block.Next.all'Address < Pool.Storage (Pool.Storage'First)'Address or
-              Block.Next.all'Address > Pool.Storage (Pool.Storage'Last)'Address
-            then
-               raise Implementation_Error;
-            end if;
-         end;
 
-         exit when CAS (Target    => Pool.Free_List'Access,
-                        Old_Value => Block,
-                        New_Value => Block.Next);
+            Block := To_Block_Access (Block_Ref, Pool);
+
+            exit when CAS (Target    => Pool.Free_List'Access,
+                           Old_Value => Block_Ref,
+                           New_Value => (Block.Next.Index,
+                                         Block_Ref.Version + 1));
+         end;
       end loop;
+
+      --  Safety test.
       declare
-         use type System.Address;
+         Head : constant Pool_Block_Access :=
+           To_Block_Access (Pool.Free_List, Pool);
       begin
-         if Pool.Free_List.all'Address < Pool.Storage (Pool.Storage'First)'Address or
-           Pool.Free_List.all'Address > Pool.Storage (Pool.Storage'Last)'Address
-         then
-            raise Implementation_Error;
-         end if;
+         null;
       end;
 
-      Storage_Address := --Block.all'Address;
+      Storage_Address :=
         Pool_Blocks.To_Address (Pool_Blocks.Object_Pointer (Block));
 
+      --  Safety test.
       declare
          use type System.Address;
       begin
@@ -112,33 +133,41 @@ package body Lock_Free_Fixed_Size_Storage_Pools is
 
       Block : Pool_Block_Access;
    begin
-      Block := Pool_Block_Access (Pool_Blocks.To_Pointer (Storage_Address));
-
-      if Storage_Address < Pool.Storage (Pool.Storage'First)'Address or
-         Storage_Address > Pool.Storage (Pool.Storage'Last)'Address
+      --  Safety test.
+      if
+        Storage_Address < Pool.Storage (Pool.Storage'First)'Address or
+        Storage_Address > Pool.Storage (Pool.Storage'Last)'Address
       then
          raise Storage_Error;
       end if;
+
+      Block :=
+        Pool_Block_Access (Pool_Blocks.To_Pointer (Storage_Address));
+
       if Block.all'Address /= Storage_Address then
          raise Implementation_Error;
       end if;
 
       loop
-         Block.Next := Pool.Free_List;
+         declare
+            Old_Head : constant Pool_Block_Ref := Pool.Free_List;
+            New_Head : constant Pool_Block_Ref :=
+              To_Block_Ref (Block, Old_Head.Version + 1, Pool);
+         begin
+            Block.Next := Old_Head;
 
-         exit when CAS (Target    => Pool.Free_List'Access,
-                        Old_Value => Block.Next,
-                        New_Value => Block);
+            exit when CAS (Target    => Pool.Free_List'Access,
+                           Old_Value => Old_Head,
+                           New_Value => New_Head);
+         end;
       end loop;
 
+      --  Safety test.
       declare
-         use type System.Address;
+         Head : constant Pool_Block_Access :=
+           To_Block_Access (Pool.Free_List, Pool);
       begin
-         if Pool.Free_List.all'Address < Pool.Storage (Pool.Storage'First)'Address or
-           Pool.Free_List.all'Address > Pool.Storage (Pool.Storage'Last)'Address
-         then
-            raise Implementation_Error;
-         end if;
+         null;
       end;
    end Deallocate;
 
@@ -153,10 +182,10 @@ package body Lock_Free_Fixed_Size_Storage_Pools is
 
    ----------------------------------------------------------------------------
    function Validate (Pool : Lock_Free_Storage_Pool)
-                     return Natural is
+                     return Block_Count is
       use type System.Address;
-      Block      : Pool_Block_Access := Pool.Free_List;
-      No_Of_Free : Natural := 0;
+      Block : Pool_Block_Access := To_Block_Access (Pool.Free_List, Pool);
+      No_Of_Free : Block_Count := 0;
    begin
       while Block /= null loop
          if Block.all'Address < Pool.Storage (Pool.Storage'First)'Address or
@@ -165,7 +194,7 @@ package body Lock_Free_Fixed_Size_Storage_Pools is
             raise Implementation_Error;
          end if;
          No_Of_Free := No_Of_Free + 1;
-         Block := Block.Next;
+         Block := To_Block_Access (Block.Next, Pool);
       end loop;
       return No_Of_Free;
    end Validate;
@@ -177,17 +206,19 @@ package body Lock_Free_Fixed_Size_Storage_Pools is
       Pool.Real_Block_Size :=
         Storage_Count'Max (Pool.Block_Size,
                            Pool_Block'Max_Size_In_Storage_Elements);
+
       Pool.Storage := new Storage_Array
         (0 .. Storage_Count (Pool.Pool_Size) * Pool.Real_Block_Size);
-      Pool.Free_List := null;
+
+      Pool.Free_List := Null_Ref;
+
       Primitives.Membar;
 
       for I in 0 .. Storage_Count (Pool.Pool_Size - 1) loop
          declare
-            Block : Pool_Block_Access :=
-              Pool_Block_Access
-               (Pool_Blocks.To_Pointer
-                (Pool.Storage (I * Pool.Real_Block_Size)'Address));
+            Block_Ref : constant Pool_Block_Ref := (Block_Index (I), 0);
+            Block     : constant Pool_Block_Access :=
+              To_Block_Access (Block_Ref, Pool);
             use type System.Address;
          begin
             if Block.all'Address /=
@@ -202,7 +233,7 @@ package body Lock_Free_Fixed_Size_Storage_Pools is
 
                exit when CAS (Target    => Pool.Free_List'Access,
                               Old_Value => Block.Next,
-                              New_Value => Block);
+                              New_Value => Block_Ref);
             end loop;
          end;
       end loop;
@@ -216,8 +247,76 @@ package body Lock_Free_Fixed_Size_Storage_Pools is
    procedure Finalize (Pool : in out Lock_Free_Storage_Pool) is
    begin
       Primitives.Membar;
-      Pool.Free_List := null;
+      Pool.Free_List := Null_Ref;
+      Primitives.Membar;
       Free (Pool.Storage);
    end Finalize;
+
+   ----------------------------------------------------------------------------
+   function To_Block_Access (X    : Pool_Block_Ref;
+                             Pool : Lock_Free_Storage_Pool)
+                            return Pool_Block_Access is
+   begin
+      if Is_Null (X) then
+         return null;
+      elsif Block_Count (X.Index) < Pool.Pool_Size then
+         declare
+            use System.Storage_Elements;
+            Block : constant Pool_Block_Access :=
+              Pool_Block_Access
+              (Pool_Blocks.To_Pointer
+               (Pool.Storage (Storage_Count (X.Index) *
+                              Pool.Real_Block_Size)'Address));
+         begin
+            --  Safety test.
+            declare
+               use type System.Address;
+            begin
+               if
+                 Block.all'Address < Pool.Storage (Pool.Storage'First)'Address
+                 or
+                 Block.all'Address > Pool.Storage (Pool.Storage'Last)'Address
+               then
+                  raise Implementation_Error;
+               end if;
+            end;
+
+            return Block;
+         end;
+      else
+         --  Invalid Block_Pool_Ref.
+         raise Constraint_Error;
+      end if;
+   end To_Block_Access;
+
+   ----------------------------------------------------------------------------
+   function To_Block_Ref (X    : Pool_Block_Access;
+                          Ver  : Version_Number;
+                          Pool : Lock_Free_Storage_Pool)
+                         return Pool_Block_Ref is
+      use System.Storage_Elements;
+
+      Block_Ref : constant Pool_Block_Ref :=
+        (Block_Index (Storage_Count ((To_Integer (X.all'Address) -
+                                      To_Integer (Pool.Storage (0)'Address))) /
+                      Pool.Real_Block_Size),
+         Ver);
+   begin
+      --  Safety test.
+      if
+        To_Integer (X.all'Address) - To_Integer (Pool.Storage (0)'Address) >
+        Integer_Address (Pool.Storage'Length)
+      then
+         raise Implementation_Error;
+      end if;
+
+      return Block_Ref;
+   end To_Block_Ref;
+
+   ----------------------------------------------------------------------------
+   function Is_Null (X : Pool_Block_Ref) return Boolean is
+   begin
+      return X.Index = Block_Index'Last;
+   end Is_Null;
 
 end Lock_Free_Fixed_Size_Storage_Pools;
--- src/Lock-Free_Storage_Pools/nbada-lock_free_fixed_size_storage_pools.ads
+++ src/Lock-Free_Storage_Pools/nbada-lock_free_fixed_size_storage_pools.ads
@@ -1,10 +1,34 @@
 -------------------------------------------------------------------------------
+--  Lock-free fixed size storage pool.
+--  Copyright (C) 2003 - 2005  Anders Gidenstam
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+--  As a special exception, if other files instantiate generics from this
+--  unit, or you link this unit with other files to produce an executable,
+--  this unit does not by itself cause the resulting executable to be
+--  covered by the GNU General Public License. This exception does not
+--  however invalidate any other reasons why the executable file might be
+--  covered by the GNU Public License.
+-------------------------------------------------------------------------------
 --                              -*- Mode: Ada -*-
--- Filename        : lock_free_fixed_size_storage_pools.ads
--- Description     : A lock-free fixed size storage pool implementation.
--- Author          : Anders Gidenstam
--- Created On      : Thu Apr  3 17:06:17 2003
--- $Id: nbada-lock_free_fixed_size_storage_pools.ads,v 1.2 2005/06/10 08:58:21 andersg Exp $
+--  Filename        : lock_free_fixed_size_storage_pools.ads
+--  Description     : A lock-free fixed size storage pool implementation.
+--  Author          : Anders Gidenstam
+--  Created On      : Thu Apr  3 17:06:17 2003
+--  $Id: nbada-lock_free_fixed_size_storage_pools.ads,v 1.3 2005/06/10 14:31:09 anders Exp $
 -------------------------------------------------------------------------------
 
 with System.Storage_Elements;
@@ -14,9 +38,11 @@ package Lock_Free_Fixed_Size_Storage_Pools is
    pragma Preelaborate (Lock_Free_Fixed_Size_Storage_Pools);
 
    ----------------------------------------------------------------------------
-   -- The pool size is in blocks.
+   --  The pool size is in blocks.
+   type Block_Count is range 0 .. 2**16 - 1;
+
    type Lock_Free_Storage_Pool
-     (Pool_Size  : Natural;
+     (Pool_Size  : Block_Count;
       Block_Size : System.Storage_Elements.Storage_Count) is
      new System.Storage_Pools.Root_Storage_Pool with private;
 
@@ -40,7 +66,7 @@ package Lock_Free_Fixed_Size_Storage_Pools is
 
    ----------------------------------------------------------------------------
    function Validate (Pool : Lock_Free_Storage_Pool)
-                     return Natural;
+                     return Block_Count;
 
    ----------------------------------------------------------------------------
    Storage_Exhausted : exception;
@@ -48,26 +74,39 @@ package Lock_Free_Fixed_Size_Storage_Pools is
 
 private
 
-   type Pool_Block;
+   --  The storage pool free-list uses 16-bit version tags to avoid
+   --  ABA-problems.
+   type Version_Number is mod 2**16;
+   type Block_Index is mod 2**16;
 
-   type Pool_Block_Access is access all Pool_Block;
-   pragma Atomic (Pool_Block_Access);
+   type Pool_Block_Ref is
+      record
+         Index   : Block_Index    := Block_Index'Last;
+         Version : Version_Number := 0;
+      end record;
+   for Pool_Block_Ref'Size use 32;
+
+   Null_Ref : constant Pool_Block_Ref := (Block_Index'Last, 0);
 
    type Pool_Block is
       record
-         Next    : aliased Pool_Block_Access;
+         Next    : aliased Pool_Block_Ref := Null_Ref;
+         pragma Atomic (Next);
       end record;
+   pragma Atomic (Pool_Block);
 
    type Storage_Array_Access is access System.Storage_Elements.Storage_Array;
 
    type Lock_Free_Storage_Pool
-     (Pool_Size  : Natural;
+     (Pool_Size  : Block_Count;
       Block_Size : System.Storage_Elements.Storage_Count) is
      new System.Storage_Pools.Root_Storage_Pool with
       record
          Storage         : Storage_Array_Access;
+         pragma Atomic (Storage);
          Real_Block_Size : System.Storage_Elements.Storage_Count;
-         Free_List       : aliased Pool_Block_Access;
+         Free_List       : aliased Pool_Block_Ref;
+         pragma Atomic (Free_List);
       end record;
 
    procedure Initialize (Pool : in out Lock_Free_Storage_Pool);
