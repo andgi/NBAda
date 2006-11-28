1930867481846eb1b6182fbbe5087138196e629b
andersg <andersg>
2006-11-28 16:52:09 +0000

    Initial version.

---
+++ src/Lock-Free_Queue/nbada-lock_free_queues.adb
@@ -0,0 +1,185 @@
+-------------------------------------------------------------------------------
+--  Lock-free Queue - An implementation of Michael and Scott's lock-free queue.
+--  Copyright (C) 2006  Anders Gidenstam
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+--  As a special exception, if other files instantiate generics from this
+--  unit, or you link this unit with other files to produce an executable,
+--  this unit does not by itself cause the resulting executable to be
+--  covered by the GNU General Public License. This exception does not
+--  however invalidate any other reasons why the executable file might be
+--  covered by the GNU Public License.
+--
+-------------------------------------------------------------------------------
+--                              -*- Mode: Ada -*-
+--  Filename        : lock_free_queues.adb
+--  Description     : An Ada implementation of Michael and Scott's
+--                    lock-free queue algorithm.
+--  Author          : Anders Gidenstam
+--  Created On      : Tue Nov 28 14:35:35 2006
+-------------------------------------------------------------------------------
+
+pragma License (Modified_GPL);
+
+with Ada.Unchecked_Deallocation;
+with Ada.Unchecked_Conversion;
+
+with Lock_Free_Growing_Storage_Pools;
+
+package body Lock_Free_Queues is
+
+   -------------------------------------------------------------------------
+   --  Storage pool for the nodes.
+   -------------------------------------------------------------------------
+
+   Node_Pool : Lock_Free_Growing_Storage_Pools.Lock_Free_Storage_Pool
+     (Block_Size => Queue_Node'Max_Size_In_Storage_Elements);
+
+   type New_Queue_Node_Access is access Queue_Node;
+   for New_Queue_Node_Access'Storage_Pool use Node_Pool;
+
+   function New_Node is new MR_Ops.Create
+     (User_Node_Access => New_Queue_Node_Access);
+
+   ----------------------------------------------------------------------------
+   --  Public operations.
+   ----------------------------------------------------------------------------
+
+   ----------------------------------------------------------------------------
+   procedure Init    (Queue : in out Queue_Type) is
+      use MR_Ops;
+      Node :  constant Private_Reference := New_Node;
+   begin
+      Store (Queue.Head'Access, Node);
+      Store (Queue.Tail'Access, Node);
+      Release (Node);
+   end Init;
+
+   ----------------------------------------------------------------------------
+   function  Dequeue (From : access Queue_Type) return Element_Type is
+      use MR_Ops;
+   begin
+      loop
+         declare
+            Head : constant Private_Reference :=
+              Dereference (From.Head'Access);
+            Tail : constant Private_Reference :=
+              Dereference (From.Tail'Access);
+            Next : constant Private_Reference :=
+              Dereference ("+" (Head).Next'Access);
+         begin
+            if Head = From.Head then
+               if Head = Tail then
+                  if Next = Null_Reference then
+                     Release (Next);
+                     Release (Tail);
+                     Release (Head);
+                     raise Queue_Empty;
+                  end if;
+
+                  Void_Compare_And_Swap (Link      => From.Tail'Access,
+                                         Old_Value => Tail,
+                                         New_Value => Next);
+               else
+                  declare
+                     Result : constant Element_Type := "+" (Next).Element;
+                  begin
+                     if
+                       Boolean_Compare_And_Swap (Link      => From.Head'Access,
+                                                 Old_Value => Head,
+                                                 New_Value => Next)
+                     then
+                        Release (Next);
+                        Release (Tail);
+                        Delete  (Head);
+                        return Result;
+                     end if;
+                  end;
+               end if;
+            end if;
+
+            Release (Next);
+            Release (Tail);
+            Release (Head);
+         end;
+      end loop;
+   end Dequeue;
+
+   ----------------------------------------------------------------------------
+   procedure Enqueue (On      : in out Queue_Type;
+                      Element : in     Element_Type) is
+      use MR_Ops;
+      Node :  constant Private_Reference := New_Node;
+   begin
+      "+" (Node).Element := Element;
+
+      loop
+         declare
+            Tail : constant Private_Reference :=
+              Dereference (On.Tail'Access);
+            Next : constant Private_Reference :=
+              Dereference ("+" (Tail).Next'Access);
+         begin
+            if Tail = On.Tail then
+               if Next = Null_Reference then
+                  if Boolean_Compare_And_Swap
+                    (Link      => "+" (Tail).Next'Access,
+                     Old_Value => Next,
+                     New_Value => Node)
+                  then
+                     Void_Compare_And_Swap (Link      => On.Tail'Access,
+                                            Old_Value => Tail,
+                                            New_Value => Node);
+                     Release (Next);
+                     Release (Tail);
+                     Release (Node);
+                     return;
+                  end if;
+               else
+                  Void_Compare_And_Swap (Link      => On.Tail'Access,
+                                         Old_Value => Tail,
+                                         New_Value => Next);
+               end if;
+            end if;
+
+            Release (Next);
+            Release (Tail);
+         end;
+      end loop;
+   end Enqueue;
+
+   ----------------------------------------------------------------------------
+   --  Private operations.
+   ----------------------------------------------------------------------------
+
+   ----------------------------------------------------------------------------
+   procedure Free (Node : access Queue_Node) is
+      procedure Reclaim is new
+        Ada.Unchecked_Deallocation (Queue_Node,
+                                    New_Queue_Node_Access);
+      function To_New_Queue_Node_Access is new
+        Ada.Unchecked_Conversion (MR_Ops.Node_Access,
+                                  New_Queue_Node_Access);
+      X : New_Queue_Node_Access :=
+        To_New_Queue_Node_Access (MR_Ops.Node_Access (Node));
+      --  This is dangerous in the general case but here we know
+      --  for sure that we have allocated all the nodes of the
+      --  Queue_Node type from the New_Queue_Node_Access pool.
+   begin
+      Reclaim (X);
+   end Free;
+
+end Lock_Free_Queues;
---
+++ src/Lock-Free_Queue/queue_test.adb
@@ -0,0 +1,318 @@
+-------------------------------------------------------------------------------
+--  Lock-free Queue Test - Test benchmark for lock-free queues.
+--
+--  Copyright (C) 2004 - 2006  Anders Gidenstam
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+--  As a special exception, if other files instantiate generics from this
+--  unit, or you link this unit with other files to produce an executable,
+--  this unit does not by itself cause the resulting executable to be
+--  covered by the GNU General Public License. This exception does not
+--  however invalidate any other reasons why the executable file might be
+--  covered by the GNU Public License.
+--
+-------------------------------------------------------------------------------
+--                              -*- Mode: Ada -*-
+--  Filename        : queue_test.adb
+--  Description     : Benchmark application for lock-free queues.
+--  Author          : Anders Gidenstam
+--  Created On      : Wed Apr 13 22:09:40 2005
+--  $Id: queue_test.adb,v 1.1 2006/11/28 16:52:09 andersg Exp $
+-------------------------------------------------------------------------------
+
+pragma License (Modified_GPL);
+
+with Process_Identification;
+with Primitives;
+
+with Ada.Text_IO;
+with Ada.Exceptions;
+
+with Ada.Real_Time;
+
+with System.Task_Info;
+
+with Lock_Free_Queues;
+
+with Ada.Command_Line;
+
+procedure Queue_Test is
+
+   package PID is
+      new Process_Identification (Max_Number_Of_Processes => 65);
+
+   type Value_Type is
+      record
+         Creator : PID.Process_ID_Type;
+         Index   : Integer;
+      end record;
+   package Queues is new Lock_Free_Queues (Element_Type  => Value_Type,
+                                           Process_Ids   => PID);
+   use Queues;
+
+   ----------------------------------------------------------------------------
+   --  Test application.
+   ----------------------------------------------------------------------------
+
+   No_Of_Elements : constant := 10_000;
+   QUEUE_FIFO_PROPERTY_VIOLATION : exception;
+
+   Output_File : Ada.Text_IO.File_Type renames
+     Ada.Text_IO.Standard_Output;
+--     Ada.Text_IO.Standard_Error;
+
+   function Pinned_Task return System.Task_Info.Task_Info_Type;
+
+   task type Producer is
+      pragma Task_Info (Pinned_Task);
+      pragma Storage_Size (1 * 1024 * 1024);
+   end Producer;
+
+   task type Consumer is
+      pragma Task_Info (Pinned_Task);
+      pragma Storage_Size (1 * 1024 * 1024);
+   end Consumer;
+
+   Queue                : aliased Queues.Queue_Type;
+
+   Start                : aliased Primitives.Unsigned_32 := 0;
+   Enqueue_Count        : aliased Primitives.Unsigned_32 := 0;
+   Dequeue_Count        : aliased Primitives.Unsigned_32 := 0;
+   No_Producers_Running : aliased Primitives.Unsigned_32 := 0;
+   No_Consumers_Running : aliased Primitives.Unsigned_32 := 0;
+
+   Task_Count : aliased Primitives.Unsigned_32 := 0;
+   function Pinned_Task return System.Task_Info.Task_Info_Type is
+   begin
+      --  GNAT/IRIX
+--        return new System.Task_Info.Thread_Attributes'
+--          (Scope       => System.Task_Info.PTHREAD_SCOPE_SYSTEM,
+--           Inheritance => System.Task_Info.PTHREAD_EXPLICIT_SCHED,
+--           Policy      => System.Task_Info.SCHED_RR,
+--           Priority    => System.Task_Info.No_Specified_Priority,
+--           Runon_CPU   =>
+--             --System.Task_Info.ANY_CPU
+--             Integer (Primitives.Fetch_And_Add (Task_Count'Access, 1))
+--           );
+      --  GNAT/Linux
+      return System.Task_Info.System_Scope;
+   end Pinned_Task;
+
+   ----------------------------------------------------------------------------
+   task body Producer is
+      No_Enqueues : Primitives.Unsigned_32 := 0;
+   begin
+      PID.Register;
+      Primitives.Fetch_And_Add (No_Producers_Running'Access, 1);
+
+      declare
+         use type Primitives.Unsigned_32;
+      begin
+         while Start = 0 loop
+            null;
+         end loop;
+      end;
+
+      declare
+         ID          : constant PID.Process_ID_Type := PID.Process_ID;
+      begin
+         for I in 1 .. No_Of_Elements loop
+            Enqueue (Queue, Value_Type'(ID, I));
+            No_Enqueues := Primitives.Unsigned_32'Succ (No_Enqueues);
+         end loop;
+
+      exception
+         when E : others =>
+            Ada.Text_IO.New_Line (Output_File);
+            Ada.Text_IO.Put_Line (Output_File,
+                                  "Producer (" &
+                                  PID.Process_ID_Type'Image (ID) &
+                                  "): raised " &
+                                  Ada.Exceptions.Exception_Name (E) &
+                                  " : " &
+                                  Ada.Exceptions.Exception_Message (E));
+            Ada.Text_IO.New_Line (Output_File);
+      end;
+      declare
+         use type Primitives.Unsigned_32;
+      begin
+         Primitives.Fetch_And_Add (Enqueue_Count'Access, No_Enqueues);
+         Primitives.Fetch_And_Add (No_Producers_Running'Access, -1);
+      end;
+      Ada.Text_IO.Put_Line (Output_File,
+                            "Producer (?): exited.");
+
+   exception
+      when E : others =>
+         Ada.Text_IO.New_Line (Output_File);
+         Ada.Text_IO.Put_Line (Output_File,
+                               "Producer (?): raised " &
+                               Ada.Exceptions.Exception_Name (E) &
+                               " : " &
+                               Ada.Exceptions.Exception_Message (E));
+         Ada.Text_IO.New_Line (Output_File);
+   end Producer;
+
+   ----------------------------------------------------------------------------
+   task body Consumer is
+      No_Dequeues : Primitives.Unsigned_32 := 0;
+   begin
+      PID.Register;
+      Primitives.Fetch_And_Add (No_Consumers_Running'Access, 1);
+
+      declare
+         ID   : constant PID.Process_ID_Type := PID.Process_ID;
+         Last : array (PID.Process_ID_Type) of Integer := (others => 0);
+         V    : Value_Type;
+         Done : Boolean := False;
+      begin
+
+         declare
+            use type Primitives.Unsigned_32;
+         begin
+            while Start = 0 loop
+               null;
+            end loop;
+         end;
+
+         loop
+
+            begin
+               V           := Dequeue (Queue'Access);
+               No_Dequeues := Primitives.Unsigned_32'Succ (No_Dequeues);
+
+               Done := False;
+
+               if V.Index <= Last (V.Creator) then
+                  raise QUEUE_FIFO_PROPERTY_VIOLATION;
+               end if;
+               Last (V.Creator) := V.Index;
+
+            exception
+               when Queues.Queue_Empty =>
+                  Ada.Text_IO.Put (".");
+                  declare
+                     use type Primitives.Unsigned_32;
+                  begin
+                     exit when Done and No_Producers_Running = 0;
+                  end;
+                  delay 0.0;
+
+                  Done := True;
+            end;
+         end loop;
+
+      exception
+         when E : others =>
+            Ada.Text_IO.New_Line (Output_File);
+            Ada.Text_IO.Put_Line (Output_File,
+                                  "Consumer (" &
+                                  PID.Process_ID_Type'Image (ID) &
+                                  "): raised " &
+                                  Ada.Exceptions.Exception_Name (E) &
+                                  " : " &
+                                  Ada.Exceptions.Exception_Message (E));
+            Ada.Text_IO.New_Line (Output_File);
+      end;
+
+      declare
+         use type Primitives.Unsigned_32;
+      begin
+         Primitives.Fetch_And_Add (Dequeue_Count'Access, No_Dequeues);
+         Primitives.Fetch_And_Add (No_Consumers_Running'Access, -1);
+      end;
+
+      Ada.Text_IO.Put_Line (Output_File,
+                            "Consumer (?): exited.");
+   exception
+      when E : others =>
+            Ada.Text_IO.New_Line (Output_File);
+            Ada.Text_IO.Put_Line (Output_File,
+                                  "Consumer (?): raised " &
+                                  Ada.Exceptions.Exception_Name (E) &
+                                  " : " &
+                                  Ada.Exceptions.Exception_Message (E));
+            Ada.Text_IO.New_Line (Output_File);
+   end Consumer;
+
+   use type Ada.Real_Time.Time;
+   T1, T2 : Ada.Real_Time.Time;
+begin
+   PID.Register;
+
+   Ada.Text_IO.Put ("Initializing: ");
+   Init (Queue);
+   Ada.Text_IO.Put_Line (" Queue ");
+
+   Ada.Text_IO.Put_Line ("Testing with producer/consumer tasks.");
+   declare
+      use type Primitives.Unsigned_32;
+--      P1, P2, P3, P4 : Producer;
+--      C1, C2, C3, C4 : Consumer;
+      P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14
+        : Producer;
+      C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14
+        : Consumer;
+   begin
+      delay 5.0;
+      T1 := Ada.Real_Time.Clock;
+      Primitives.Fetch_And_Add (Start'Access, 1);
+   end;
+--     declare
+--        C1 : Consumer;
+--     begin
+--        null;
+--     end;
+   T2 := Ada.Real_Time.Clock;
+
+   delay 1.0;
+   Ada.Text_IO.Put_Line ("Enqueue count: " &
+                         Primitives.Unsigned_32'Image (Enqueue_Count));
+   Ada.Text_IO.Put_Line ("Dequeue count: " &
+                         Primitives.Unsigned_32'Image (Dequeue_Count));
+   Ada.Text_IO.Put_Line ("Elapsed time:" &
+                         Duration'Image (Ada.Real_Time.To_Duration (T2 - T1)));
+
+   Ada.Text_IO.Put_Line ("Emptying queue.");
+   delay 5.0;
+
+   declare
+      V : Value_Type;
+   begin
+      loop
+         V := Dequeue (Queue'Access);
+         Ada.Text_IO.Put_Line (Output_File,
+                               "Dequeue() = (" &
+                               PID.Process_ID_Type'Image (V.Creator) & ", " &
+                               Integer'Image (V.Index) & ")");
+         Primitives.Fetch_And_Add (Dequeue_Count'Access, 1);
+      end loop;
+   exception
+      when E : others =>
+         Ada.Text_IO.New_Line (Output_File);
+         Ada.Text_IO.Put_Line (Output_File,
+                               "raised " &
+                               Ada.Exceptions.Exception_Name (E) &
+                               " : " &
+                               Ada.Exceptions.Exception_Message (E));
+         Ada.Text_IO.New_Line (Output_File);
+
+         Ada.Text_IO.Put_Line ("Final enqueue count: " &
+                               Primitives.Unsigned_32'Image (Enqueue_Count));
+         Ada.Text_IO.Put_Line ("Final dequeue count: " &
+                               Primitives.Unsigned_32'Image (Dequeue_Count));
+   end;
+end Queue_Test;
