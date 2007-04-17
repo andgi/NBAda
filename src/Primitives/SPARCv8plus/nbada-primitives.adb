02ae778f294beef9ad7c6a606adf7c8b31a7a7bb
andersg <andersg>
2007-04-17 17:14:28 +0000

    Created from the SPARCv9 directory. Changed to support exactly 32-bit SPARC v8+.

--- src/Primitives/SPARCv8plus/nbada-primitives.adb
+++ src/Primitives/SPARCv8plus/nbada-primitives.adb
@@ -28,16 +28,17 @@
 --  Description     : Synchronization primitives.
 --  Author          : Anders Gidenstam
 --  Created On      : Fri Jul  5 14:53:50 2002
---  $Id: nbada-primitives.adb,v 1.11 2007/04/11 17:15:50 andersg Exp $
+--  $Id: nbada-primitives.adb,v 1.12 2007/04/17 17:14:28 andersg Exp $
 -------------------------------------------------------------------------------
 
 with System.Machine_Code;
 with Ada.Characters.Latin_1;
+with Ada.Unchecked_Conversion;
 
 package body Primitives is
 
    ----------------------------------------------------------------------------
-   --  Synchronization primitives for SPARC v9.
+   --  Synchronization primitives for 32-bit SPARC v9.
    --
    --  Requires the flags '-cargs -Wa,-xarch=v8plus' to gnatmake.
    ----------------------------------------------------------------------------
@@ -48,6 +49,90 @@ package body Primitives is
    subtype Always_True is Boolean range True .. True;
    type Assertion (Assert : Always_True) is
      null record;
+
+
+   ----------------------------------------------------------------------------
+   --  Primitives for the platform's standard word size.
+   ----------------------------------------------------------------------------
+
+   ----------------------------------------------------------------------------
+   function Standard_Atomic_Read (Target : access Element) return Element is
+      function Atomic_Read is new Atomic_Read_32 (Element);
+   begin
+      return Atomic_Read (Target);
+   end Standard_Atomic_Read;
+
+   ----------------------------------------------------------------------------
+   procedure Standard_Atomic_Write (Target : access Element;
+                                    Value  : in     Element) is
+      procedure Atomic_Write is new Atomic_Write_32 (Element);
+   begin
+      Atomic_Write (Target, Value);
+   end Standard_Atomic_Write;
+
+   ----------------------------------------------------------------------------
+   procedure Standard_Compare_And_Swap (Target    : access Element;
+                                        Old_Value : in     Element;
+                                        New_Value : in out Element) is
+      procedure Compare_And_Swap is new Compare_And_Swap_32 (Element);
+   begin
+      Compare_And_Swap (Target, Old_Value, New_Value);
+   end Standard_Compare_And_Swap;
+
+   ----------------------------------------------------------------------------
+   function Standard_Boolean_Compare_And_Swap (Target    : access Element;
+                                               Old_Value : in     Element;
+                                               New_Value : in     Element)
+                                              return Boolean is
+      function Compare_And_Swap is new Boolean_Compare_And_Swap_32 (Element);
+   begin
+      return Compare_And_Swap (Target, Old_Value, New_Value);
+   end Standard_Boolean_Compare_And_Swap;
+
+   ----------------------------------------------------------------------------
+   procedure Standard_Void_Compare_And_Swap (Target    : access Element;
+                                             Old_Value : in     Element;
+                                             New_Value : in     Element) is
+      procedure Compare_And_Swap is new Void_Compare_And_Swap_32 (Element);
+   begin
+      Compare_And_Swap (Target, Old_Value, New_Value);
+   end Standard_Void_Compare_And_Swap;
+
+   ----------------------------------------------------------------------------
+   procedure Fetch_And_Add (Target    : access Standard_Unsigned;
+                            Increment : in     Standard_Unsigned) is
+      function To_U32 is
+         new Ada.Unchecked_Conversion (Standard_Unsigned, Unsigned_32);
+      type SU_Access is access all Standard_Unsigned;
+      type U32_Access is access all Unsigned_32;
+      function To_U32_Access is
+         new Ada.Unchecked_Conversion (SU_Access, U32_Access);
+   begin
+      Fetch_And_Add_32 (To_U32_Access (SU_Access (Target)),
+                        To_U32 (Increment));
+   end Fetch_And_Add;
+
+   ----------------------------------------------------------------------------
+   function  Fetch_And_Add (Target    : access Standard_Unsigned;
+                            Increment : in     Standard_Unsigned)
+                           return Standard_Unsigned is
+      function To_U32 is
+         new Ada.Unchecked_Conversion (Standard_Unsigned, Unsigned_32);
+      function To_SU is
+         new Ada.Unchecked_Conversion (Unsigned_32, Standard_Unsigned);
+      type SU_Access is access all Standard_Unsigned;
+      type U32_Access is access all Unsigned_32;
+      function To_U32_Access is
+         new Ada.Unchecked_Conversion (SU_Access, U32_Access);
+   begin
+      return To_SU (Fetch_And_Add_32 (To_U32_Access (SU_Access (Target)),
+                                      To_U32 (Increment)));
+   end Fetch_And_Add;
+
+
+   ----------------------------------------------------------------------------
+   --  32-bit primitives.
+   ----------------------------------------------------------------------------
 
    ----------------------------------------------------------------------------
    function Atomic_Read_32 (Target : access Element) return Element is
@@ -153,85 +238,8 @@ package body Primitives is
    end Void_Compare_And_Swap_32;
 
    ----------------------------------------------------------------------------
-   --  My GCC does not generate 64-bit aware code, so 64-bit objects cannot be
-   --  handled atomically.
-   ----------------------------------------------------------------------------
-
-   ----------------------------------------------------------------------------
-   procedure Compare_And_Swap_64 (Target    : access Element;
-                                  Old_Value : in     Element;
-                                  New_Value : in out Element) is
-      use Ada.Characters.Latin_1;
-      type Element_Access is access all Element;
-
-      A1 : Assertion (Assert => Element'Object_Size = 64);
-   begin
-      raise Not_Implemented;
---       System.Machine_Code.Asm
---         (Template =>
---            "!#BEGIN Compare_And_Swap_64" & LF & HT &
---            "membar #LoadLoad | #StoreStore | #LoadStore | #StoreLoad" &LF &HT&
---            "mov %3, %%l0"                & LF & HT &   -- l0 <- %3
---            "casx [%1], %2, %%l0"         & LF & HT &   -- Compare & swap
---            "mov %%l0, %0"                & LF & HT &   -- %0 <- l0
---            "membar #LoadLoad | #StoreStore | #LoadStore | #StoreLoad" &LF &HT&
---            "!#END Compare_And_Swap_64",
---          Outputs  => Element'Asm_Output ("=r", New_Value), -- %0 = New_Value
---          Inputs   => (Element_Access'Asm_Input ("r",       -- %1 = Target
---                                                 Element_Access (Target)),
---                       Element'Asm_Input ("r", Old_Value),  -- %2 = Old_Value
---                       Element'Asm_Input ("r", New_Value)), -- %3 = New_Value
---          Clobber  => "l0",
---          Volatile => True);
-   end Compare_And_Swap_64;
-
-   ----------------------------------------------------------------------------
-   function Boolean_Compare_And_Swap_64 (Target    : access Element;
-                                         Old_Value : in     Element;
-                                         New_Value : in     Element)
-                                        return Boolean is
-      use Ada.Characters.Latin_1;
-      type Element_Access is access all Element;
-
-      A1 : Assertion (Assert => Element'Object_Size = 64);
-      Tmp : Element;
-   begin
-      raise Not_Implemented;
---       System.Machine_Code.Asm
---         (Template =>
---            "!#BEGIN Boolean_Compare_And_Swap_64" & LF & HT &
---            "membar #LoadLoad | #StoreStore | #LoadStore | #StoreLoad" &LF &HT&
---            "mov %3, %%l0"                & LF & HT &   -- l0 <- %3
---            "casx [%1], %2, %%l0"         & LF & HT &   -- Compare & swap
---            "mov %%l0, %0"                & LF & HT &   -- %0 <- l0
---            "membar #LoadLoad | #StoreStore | #LoadStore | #StoreLoad" &LF &HT&
---            "!#END Boolean_Compare_And_Swap_64",
---          Outputs  => Element'Asm_Output ("=r", Tmp),       -- %0 = Tmp
---          Inputs   => (Element_Access'Asm_Input ("r",       -- %1 = Target
---                                                 Element_Access (Target)),
---                       Element'Asm_Input ("r", Old_Value),  -- %2 = Old_Value
---                       Element'Asm_Input ("r", New_Value)), -- %3 = New_Value
---          Clobber  => "l0",
---          Volatile => True);
---       return Tmp = Old_Value;
-      return False;
-   end Boolean_Compare_And_Swap_64;
-
-   ----------------------------------------------------------------------------
-   procedure Void_Compare_And_Swap_64 (Target    : access Element;
-                                       Old_Value : in     Element;
-                                       New_Value : in     Element) is
-      use Ada.Characters.Latin_1;
-      type Element_Access is access all Element;
-
-      A1 : Assertion (Assert => Element'Object_Size = 64);
-   begin
-      raise Not_Implemented;
-   end Void_Compare_And_Swap_64;
-
-   ----------------------------------------------------------------------------
-   procedure Fetch_And_Add (Target    : access Unsigned_32;
-                            Increment : in     Unsigned_32) is
+   procedure Fetch_And_Add_32 (Target    : access Unsigned_32;
+                               Increment : in     Unsigned_32) is
       use Ada.Characters.Latin_1;
       type Unsigned_32_Access is access all Unsigned_32;
    begin
@@ -254,12 +262,12 @@ package body Primitives is
                                              Increment)),
          Clobber  => "l0,l1",
          Volatile => True);
-   end Fetch_And_Add;
+   end Fetch_And_Add_32;
 
    ----------------------------------------------------------------------------
-   function Fetch_And_Add (Target    : access Unsigned_32;
-                           Increment : in     Unsigned_32)
-                          return Unsigned_32 is
+   function Fetch_And_Add_32 (Target    : access Unsigned_32;
+                              Increment : in     Unsigned_32)
+                             return Unsigned_32 is
       use Ada.Characters.Latin_1;
       type Unsigned_32_Access is access all Unsigned_32;
 
@@ -287,7 +295,84 @@ package body Primitives is
          Clobber  => "l0,l1",
          Volatile => True);
       return Tmp;
-   end Fetch_And_Add;
+   end Fetch_And_Add_32;
+
+
+   ----------------------------------------------------------------------------
+   --  64-bit primitives.
+   ----------------------------------------------------------------------------
+
+   ----------------------------------------------------------------------------
+   --  My GCC does not generate 64-bit aware code, so 64-bit objects cannot be
+   --  handled atomically.
+   ----------------------------------------------------------------------------
+
+   ----------------------------------------------------------------------------
+   function Atomic_Read_64 (Target : access Element) return Element is
+   begin
+      raise Not_Implemented;
+   end Atomic_Read_64;
+
+   ----------------------------------------------------------------------------
+   procedure Atomic_Write_64 (Target : access Element;
+                              Value  : in     Element) is
+   begin
+      raise Not_Implemented;
+   end Atomic_Write_64;
+
+   ----------------------------------------------------------------------------
+   procedure Compare_And_Swap_64 (Target    : access Element;
+                                  Old_Value : in     Element;
+                                  New_Value : in out Element) is
+      use Ada.Characters.Latin_1;
+      type Element_Access is access all Element;
+
+      A1 : Assertion (Assert => Element'Object_Size = 64);
+   begin
+      raise Not_Implemented;
+   end Compare_And_Swap_64;
+
+   ----------------------------------------------------------------------------
+   function Boolean_Compare_And_Swap_64 (Target    : access Element;
+                                         Old_Value : in     Element;
+                                         New_Value : in     Element)
+                                        return Boolean is
+      use Ada.Characters.Latin_1;
+      type Element_Access is access all Element;
+
+      A1 : Assertion (Assert => Element'Object_Size = 64);
+   begin
+      raise Not_Implemented;
+      return False;
+   end Boolean_Compare_And_Swap_64;
+
+   ----------------------------------------------------------------------------
+   procedure Void_Compare_And_Swap_64 (Target    : access Element;
+                                       Old_Value : in     Element;
+                                       New_Value : in     Element) is
+      use Ada.Characters.Latin_1;
+      type Element_Access is access all Element;
+
+      A1 : Assertion (Assert => Element'Object_Size = 64);
+   begin
+      raise Not_Implemented;
+   end Void_Compare_And_Swap_64;
+
+   ----------------------------------------------------------------------------
+   procedure Fetch_And_Add_64 (Target    : access Unsigned_64;
+                               Increment : in     Unsigned_64) is
+   begin
+      raise Not_Implemented;
+   end Fetch_And_Add_64;
+
+   ----------------------------------------------------------------------------
+   function Fetch_And_Add_64 (Target    : access Unsigned_64;
+                              Increment : in     Unsigned_64)
+                             return Unsigned_64 is
+   begin
+      raise Not_Implemented;
+      return 0;
+   end Fetch_And_Add_64;
 
    ----------------------------------------------------------------------------
    procedure Membar is
