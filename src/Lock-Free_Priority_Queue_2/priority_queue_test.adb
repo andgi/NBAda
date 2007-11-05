fce21794906cb37f92132022813cdc651c137543
andersg <andersg>
2007-11-05 19:46:43 +0000

    Improved the test application.

--- src/Lock-Free_Priority_Queue_2/priority_queue_test.adb
+++ src/Lock-Free_Priority_Queue_2/priority_queue_test.adb
@@ -24,20 +24,21 @@
 --  Description     : Test of non-blocking priority queue.
 --  Author          : Anders Gidenstam
 --  Created On      : Thu Feb 20 16:39:08 2003
---  $Id: priority_queue_test.adb,v 1.3 2007/10/31 17:21:54 andersg Exp $
+--  $Id: priority_queue_test.adb,v 1.4 2007/11/05 19:46:43 andersg Exp $
 -------------------------------------------------------------------------------
 
 pragma License (GPL);
 
+with NBAda.Primitives;
+
 with Ada.Text_IO;
 with Ada.Exceptions;
-
 with Ada.Real_Time;
+with Ada.Command_Line;
+
 with System.Task_Info;
 
 with My_Priority_Queue;
-
-with NBAda.Primitives;
 
 procedure Priority_Queue_Test is
 
@@ -50,14 +51,13 @@ procedure Priority_Queue_Test is
    --  Test application.
    ----------------------------------------------------------------------------
 
-   No_Of_Elements : constant := 100;
-
-   Output_File : Ada.Text_IO.File_Type renames
-     Ada.Text_IO.Standard_Output;
---     Ada.Text_IO.Standard_Error;
+   No_Of_Elements : constant := 1000;
 
    ----------------------------------------------------------------------------
    function Pinned_Task return System.Task_Info.Task_Info_Type;
+   procedure Print_Usage;
+   procedure Put_Line (S : in String);
+   procedure Put (S : in String);
 
    ----------------------------------------------------------------------------
    task type Producer is
@@ -82,7 +82,6 @@ procedure Priority_Queue_Test is
    pragma Atomic (No_Consumers_Running);
 
 
-   --   Task_Count : aliased Primitives.Unsigned_32 := 0;
    ----------------------------------------------------------------------------
    function Pinned_Task return System.Task_Info.Task_Info_Type is
    begin
@@ -127,15 +126,14 @@ procedure Priority_Queue_Test is
 
       exception
          when E : others =>
-            Ada.Text_IO.New_Line (Output_File);
-            Ada.Text_IO.Put_Line (Output_File,
-                                  "Producer (" &
+            Ada.Text_IO.New_Line;
+            Ada.Text_IO.Put_Line ("Producer (" &
                                   PID.Process_ID_Type'Image (ID) &
                                   "): raised " &
                                   Ada.Exceptions.Exception_Name (E) &
                                   " : " &
                                   Ada.Exceptions.Exception_Message (E));
-            Ada.Text_IO.New_Line (Output_File);
+            Ada.Text_IO.New_Line;
       end;
 
       declare
@@ -145,21 +143,19 @@ procedure Priority_Queue_Test is
          Primitives.Fetch_And_Add_32 (Insert_Count'Access, No_Inserts);
          Primitives.Fetch_And_Add_32 (No_Producers_Running'Access, -1);
 
-         Ada.Text_IO.Put_Line (Output_File,
-                               "Producer (" &
-                               PID.Process_ID_Type'Image (ID) &
-                               "): exited.");
+         Put_Line ("Producer (" &
+                   PID.Process_ID_Type'Image (ID) &
+                   "): exited.");
       end;
 
    exception
       when E : others =>
-         Ada.Text_IO.New_Line (Output_File);
-         Ada.Text_IO.Put_Line (Output_File,
-                               "Producer (?): raised " &
+         Ada.Text_IO.New_Line;
+         Ada.Text_IO.Put_Line("Producer (?): raised " &
                                Ada.Exceptions.Exception_Name (E) &
                                " : " &
                                Ada.Exceptions.Exception_Message (E));
-         Ada.Text_IO.New_Line (Output_File);
+         Ada.Text_IO.New_Line;
          declare
             use type Primitives.Unsigned_32;
          begin
@@ -169,7 +165,6 @@ procedure Priority_Queue_Test is
 
    ----------------------------------------------------------------------------
    task body Consumer is
-      Min        : Value_Type;
       No_Deletes : Primitives.Unsigned_32 := 0;
       Done       : Boolean := False;
    begin
@@ -187,17 +182,18 @@ procedure Priority_Queue_Test is
       loop
          declare
             use type Primitives.Unsigned_32;
+            Min : Value_Type;
          begin
             Delete_Min (My_Q, Min);
             No_Deletes := Primitives.Unsigned_32'Succ (No_Deletes);
-            Ada.Text_IO.Put_Line ("Consumer: Delete_Min: " &
-                                  Image (Min));
+--            Ada.Text_IO.Put_Line ("Consumer: Delete_Min: " &
+--                                  Image (Min));
 
             Done := False;
 
          exception
             when My_Priority_Queue.Priority_Queues.Queue_Empty =>
-               Ada.Text_IO.Put (".");
+               Put (".");
 
                exit when Done and No_Producers_Running = 0;
 
@@ -214,44 +210,151 @@ procedure Priority_Queue_Test is
          Primitives.Fetch_And_Add_32 (Delete_Min_Count'Access, No_Deletes);
          Primitives.Fetch_And_Add_32 (No_Consumers_Running'Access, -1);
 
-         Ada.Text_IO.Put_Line (Output_File,
-                               "Consumer (" &
-                               PID.Process_ID_Type'Image (ID) &
-                               "): exited.");
+         Put_Line ("Consumer (" &
+                   PID.Process_ID_Type'Image (ID) &
+                   "): exited.");
       end;
 
    exception
       when E : others =>
-         Ada.Text_IO.New_Line (Ada.Text_IO.Standard_Error);
-         Ada.Text_IO.Put_Line (Ada.Text_IO.Standard_Error,
-                               "Consumer: raised " &
+         Ada.Text_IO.New_Line;
+         Ada.Text_IO.Put_Line ("Consumer: raised " &
                                Ada.Exceptions.Exception_Name (E) &
                                " : " &
                                Ada.Exceptions.Exception_Message (E));
-         Ada.Text_IO.New_Line (Ada.Text_IO.Standard_Error);
+         Ada.Text_IO.New_Line;
    end Consumer;
 
+   ----------------------------------------------------------------------
+   procedure Print_Usage is
+   begin
+      Ada.Text_IO.Put_Line
+        ("Usage: " &
+         Ada.Command_Line.Command_Name &
+         " [OPTION] ");
+      Ada.Text_IO.Put_Line
+        ("  -h             Print this message.");
+      Ada.Text_IO.Put_Line
+        ("  -p <#threads>  Set the number of producer threads.");
+      Ada.Text_IO.Put_Line
+        ("  -c <#threads>  Set the number of consumer threads.");
+      Ada.Text_IO.Put_Line
+        ("  -s             Single line output.");
+   end Print_Usage;
+
+   ----------------------------------------------------------------------
+   Silent : Boolean := False;
+
+   procedure Put_Line (S : in String) is
+   begin
+      if not Silent then
+         Ada.Text_IO.Put_Line (S);
+      end if;
+   end Put_Line;
+   procedure Put (S : in String) is
+   begin
+      if not Silent then
+         Ada.Text_IO.Put (S);
+      end if;
+   end Put;
+
+   ----------------------------------------------------------------------------
+   use type Ada.Real_Time.Time;
+   T1, T2 : Ada.Real_Time.Time;
+
+   No_Producers : Natural := 4;
+   No_Consumers : Natural := 4;
 begin
    PID.Register;
+
+   --  Parse command line.
+   declare
+      N : Natural := 1;
+   begin
+      while N <= Ada.Command_Line.Argument_Count loop
+
+         if Ada.Command_Line.Argument (N) = "-h" then
+            Print_Usage;
+            return;
+         elsif Ada.Command_Line.Argument (N) = "-p" then
+            declare
+               T : Natural;
+            begin
+               N := N + 1;
+               T := Integer'Value (Ada.Command_Line.Argument (N));
+               No_Producers := T;
+            end;
+         elsif Ada.Command_Line.Argument (N) = "-c" then
+            declare
+               T : Natural;
+            begin
+               N := N + 1;
+               T := Integer'Value (Ada.Command_Line.Argument (N));
+               No_Consumers := T;
+            end;
+         elsif Ada.Command_Line.Argument (N) = "-s" then
+            Silent := True;
+         else
+            Ada.Text_IO.Put_Line ("Unknown option.");
+            Ada.Text_IO.New_Line;
+            Print_Usage;
+            return;
+         end if;
+
+         N := N + 1;
+      end loop;
+   end;
+
+   Put ("Initializing: ");
    Initialize (My_Q);
-   Ada.Text_IO.Put_Line ("Verifying priority queue...");
+   Put_Line (" Priority Queue ");
+
+   Put_Line ("Verifying priority queue...");
    Verify (My_Q, Print => True);
+
    delay 1.0;
-   Ada.Text_IO.Put_Line ("Testing with producer/consumer tasks...");
+   Put_Line ("Testing with " &
+             Integer'Image (No_Producers) & " producer and " &
+             Integer'Image (No_Consumers) & " consumer tasks.");
 
    declare
-      P1, P2, P3, P4
-        : Producer;
---      C1, C2--, C3
---        : Consumer;
+      use type Primitives.Unsigned_32;
+      Producer_Array : array (1 .. No_Producers) of Producer;
+      Consumer_Array : array (1 .. No_Consumers) of Consumer;
    begin
-      Start := 1;
+      if Producer_Array'First = Consumer_Array'Last then  --  Silence warnings.
+         null;
+      end if;
+
+      delay 5.0;
+      T1 := Ada.Real_Time.Clock;
+      Primitives.Fetch_And_Add_32 (Start'Access, 1);
    end;
-   Ada.Text_IO.Put_Line ("Verifying priority queue...");
+   T2 := Ada.Real_Time.Clock;
+
+   delay 1.0;
+   Put_Line ("Insert count: " &
+             Primitives.Unsigned_32'Image (Insert_Count));
+   Put_Line ("Delete_Min count: " &
+             Primitives.Unsigned_32'Image (Delete_Min_Count));
+   Put_Line ("Elapsed time:" &
+             Duration'Image (Ada.Real_Time.To_Duration (T2 - T1)) & " sec.");
+
+   if Silent then
+      Ada.Text_IO.Put
+        (Integer'Image (No_Producers)  & "  " &
+         Integer'Image (No_Consumers)  & "  " &
+         Primitives.Unsigned_32'Image (Insert_Count) & "  " &
+         Primitives.Unsigned_32'Image (Delete_Min_Count)  & "  " &
+         Duration'Image (Ada.Real_Time.To_Duration (T2 - T1)) &
+         "  ");
+   end if;
+
+   Put_Line ("Verifying priority queue...");
    Verify (My_Q, Print => False);
 
    delay 5.0;
-   Ada.Text_IO.Put_Line ("Emptying queue.");
+   Put_Line ("Emptying queue.");
 
    declare
       Last : Value_Type := (1, Integer'First);
@@ -261,14 +364,12 @@ begin
          Delete_Min (My_Q, Min);
          Primitives.Fetch_And_Add_32 (Delete_Min_Count'Access, 1);
 
-         Ada.Text_IO.Put_Line ("Heap_Test: Delete_Min: " &
+         Ada.Text_IO.Put_Line ("Delete_Min: " &
                                Image (Min));
 
          if Min < Last then
-            Ada.Text_IO.Put_Line ("Heap_Test: Heap property failure! ");
-            Ada.Text_IO.Put_Line (Ada.Text_IO.Standard_Error,
-                                  "Heap_Test: Heap property failure! ");
-            Ada.Text_IO.Put_Line ("Heap_Test: Last " &
+            Ada.Text_IO.Put_Line ("Heap property failure! ");
+            Ada.Text_IO.Put_Line ("Last " &
                                   Image (Last) &
                                   " got " &
                                   Image (Min) &
@@ -284,13 +385,18 @@ begin
          null;
    end;
 
-   Ada.Text_IO.Put_Line ("Final insert count: " &
-                         Primitives.Unsigned_32'Image (Insert_Count));
-   Ada.Text_IO.Put_Line ("Final delete_min count: " &
-                         Primitives.Unsigned_32'Image (Delete_Min_Count));
+   Put_Line ("Final insert count: " &
+             Primitives.Unsigned_32'Image (Insert_Count));
+   Put_Line ("Final delete_min count: " &
+             Primitives.Unsigned_32'Image (Delete_Min_Count));
 
+   if Silent then
+      Ada.Text_IO.Put (Primitives.Unsigned_32'Image (Insert_Count) & "  " &
+                       Primitives.Unsigned_32'Image (Delete_Min_Count));
+      Ada.Text_IO.New_Line;
+   end if;
 
-   Ada.Text_IO.Put_Line ("Verifying priority queue...");
+   Put_Line ("Verifying priority queue...");
    Verify (My_Q, Print => True);
 
 end Priority_Queue_Test;
