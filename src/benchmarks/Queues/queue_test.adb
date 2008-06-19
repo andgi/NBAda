6eb83b1567790b5a0a04dde0bd1d8c1a3ebc24cf
andersg <andersg>
2008-06-19 17:06:16 +0000

    Updated for GNAT GPL 2008.

--- src/benchmarks/Queues/queue_test.adb
+++ src/benchmarks/Queues/queue_test.adb
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------
 --  Lock-free Queue Test - Test benchmark for lock-free queues.
 --
---  Copyright (C) 2004 - 2007  Anders Gidenstam
+--  Copyright (C) 2004 - 2008  Anders Gidenstam
 --
 --  This program is free software; you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
 --  Description     : Benchmark application for lock-free queues.
 --  Author          : Anders Gidenstam
 --  Created On      : Wed Apr 13 22:09:40 2005
---  $Id: queue_test.adb,v 1.14 2007/09/12 15:35:18 andersg Exp $
+--  $Id: queue_test.adb,v 1.15 2008/06/19 17:06:16 andersg Exp $
 -------------------------------------------------------------------------------
 
 pragma License (GPL);
@@ -66,47 +66,30 @@ procedure Queue_Test is
    No_Of_Elements : constant := 100_000;
    QUEUE_FIFO_PROPERTY_VIOLATION : exception;
 
-   function Pinned_Task return System.Task_Info.Task_Info_Type;
    procedure Print_Usage;
    procedure Put_Line (S : in String);
    procedure Put (S : in String);
 
    task type Producer is
-      pragma Task_Info (Pinned_Task);
       pragma Storage_Size (1 * 1024 * 1024);
    end Producer;
 
    task type Consumer is
-      pragma Task_Info (Pinned_Task);
       pragma Storage_Size (1 * 1024 * 1024);
    end Consumer;
 
    Queue                : aliased Queues.Queue_Type;
 
    Start                : aliased Primitives.Unsigned_32 := 0;
+   pragma Atomic (Start);
    Enqueue_Count        : aliased Primitives.Unsigned_32 := 0;
+   pragma Atomic (Enqueue_Count);
    Dequeue_Count        : aliased Primitives.Unsigned_32 := 0;
+   pragma Atomic (Dequeue_Count);
    No_Producers_Running : aliased Primitives.Unsigned_32 := 0;
+   pragma Atomic (No_Producers_Running);
    No_Consumers_Running : aliased Primitives.Unsigned_32 := 0;
-
---   Task_Count : aliased Primitives.Unsigned_32 := 0;
-   function Pinned_Task return System.Task_Info.Task_Info_Type is
-   begin
-      --  GNAT/IRIX
---        return new System.Task_Info.Thread_Attributes'
---          (Scope       => System.Task_Info.PTHREAD_SCOPE_SYSTEM,
---           Inheritance => System.Task_Info.PTHREAD_EXPLICIT_SCHED,
---           Policy      => System.Task_Info.SCHED_RR,
---           Priority    => System.Task_Info.No_Specified_Priority,
---           Runon_CPU   =>
---             --System.Task_Info.ANY_CPU
---             Integer (Primitives.Fetch_And_Add_32 (Task_Count'Access, 1))
---           );
-      --  GNAT/Linux
-      return System.Task_Info.System_Scope;
-      --  GNAT/Solaris
---      return System.Task_Info.New_Bound_Thread_Attributes;
-   end Pinned_Task;
+   pragma Atomic (No_Consumers_Running);
 
    ----------------------------------------------------------------------------
    task body Producer is
