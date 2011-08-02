f8211001b2c9472a9c75c04365ae3f07dd5c41be
Anders Gidenstam <anders@gidenstam.org>
2011-08-02 12:56:46 +0200

    Added a module, NBAda.Per_Thread_Storage.Local, for per-object thread local storage.

---
+++ src/common/nbada-per_thread_storage-local.adb
@@ -0,0 +1,43 @@
+-------------------------------------------------------------------------------
+--  Per-object thread local storage.
+--  Copyright (C) 2011  Anders Gidenstam
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+-------------------------------------------------------------------------------
+pragma Style_Checks (Off);
+-------------------------------------------------------------------------------
+--                              -*- Mode: Ada -*-
+--  Filename        : nbada-per_thread_storage-local.adb
+--  Description     : A simple implementation of thread local storage.
+--  Author          : Anders Gidenstam
+--  Created On      : Thu Jun 07 19:55:00 2011
+-------------------------------------------------------------------------------
+pragma Style_Checks (All_Checks);
+
+pragma License (GPL);
+
+package body NBAda.Per_Thread_Storage.Local is
+
+   function Get (Source : Storage) return Element_Access is
+      ID : constant Process_Ids.Process_ID_Type := Process_Ids.Process_ID;
+   begin
+      if Source.Element (ID) = null then
+         Source.Mutable.Self.Element (ID) := new Element_Type;
+      end if;
+      return Source.Element (ID);
+   end Get;
+
+end NBAda.Per_Thread_Storage.Local;
---
+++ src/common/nbada-per_thread_storage.ads
@@ -0,0 +1,35 @@
+-------------------------------------------------------------------------------
+--  NBAda - A library of non-blocking algorithms and data structures.
+--
+--  Copyright (C) 2011  Anders Gidenstam
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+-------------------------------------------------------------------------------
+--                              -*- Mode: Ada -*-
+--  Filename        : nbada-per_thread_storage.ads
+--  Description     : NBAda - A library of non-blocking algorithms and
+--                    data structures.
+--  Author          : Anders Gidenstam
+--  Created On      : Thu Aug 01 19:01:08 2011
+-------------------------------------------------------------------------------
+
+pragma License (GPL);
+
+package NBAda.Per_Thread_Storage is
+
+   pragma Pure (NBAda.Per_Thread_Storage);
+
+end NBAda.Per_Thread_Storage;
